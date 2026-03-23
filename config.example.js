211679320ad2937a1f416f0c4d64c8f51050c3cf
Krishna8167 <krishnasharma816757@gmail.com>
2026-03-23 16:38:37 +0530

    Demo pushedd to remote

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// config.example.js
+
+export const APP_CONFIG = {
+  DATABASE_URL: "YOUR_FIREBASE_DATABASE_URL"
+};
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-let myTabs = []
+import { APP_CONFIG } from "./config.js";
+console.log("CONFIG:", APP_CONFIG);
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js"
+import { getDatabase,
+            ref,
+            push,
+            onValue,
+            remove } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-database.js"
+
+    const firebaseConfig = {
+        databaseURL: APP_CONFIG.DATABASE_URL
+    }
+
+      const app = initializeApp(firebaseConfig);
+      const database = getDatabase(app)
+      const referenceInDB = ref(database, "tab-leads")
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
 
-const tabsFromLocalStorage = JSON.parse(localStorage.getItem("myTabs"))
 
 function isValidURL(url) {
     try {
@@ -16,27 +30,6 @@ function isValidURL(url) {
         return false
     }
 }
-
-if (tabsFromLocalStorage) {
-    myTabs = tabsFromLocalStorage
-    render(myTabs)
-}
-
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        const tab = {
-            url: tabs[0].url,
-            title: tabs[0].title
-        }
-
-        if (!myTabs.some(t => t.url === tab.url)) {
-            myTabs.push(tab)
-        }
-
-        localStorage.setItem("myTabs", JSON.stringify(myTabs))
-        render(myTabs)
-    })
-})
 
 function render(tabs) {
     ulEl.innerHTML = ""
@@ -55,30 +48,21 @@ function render(tabs) {
     }
 }
 
+onValue(referenceInDB, function(snapshot) {
+     const snapshotValues = snapshot.val()
+
+     const leads = Object.values(snapshotValues)
+     console.log(leads)
+
+     render(leads)
+})
+
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.removeItem("myTabs")
-    myTabs = []
-    render(myTabs)
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
 })
 
 inputBtn.addEventListener("click", function() {
-    let value = inputEl.value.trim()
-
-    if (!value.startsWith("http://") && !value.startsWith("https://")) {
-        value = "https://" + value
-    }
-
-    if (isValidURL(value) && !myTabs.some(t => t.url === value)) {
-        myTabs.push({
-            url: value,
-            title: value
-        })
-
-        localStorage.setItem("myTabs", JSON.stringify(myTabs))
-        render(myTabs)
-    } else {
-        alert("Enter a valid and unique URL")
-    }
-
+    push(referenceInDB, inputEl.value)
     inputEl.value = ""
 })
